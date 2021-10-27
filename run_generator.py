301727a24dfae36ad0b234747dcc8a64d62819ec
mabarass01 <93286353+mabarass01@users.noreply.github.com>
2021-10-27 21:30:24 +0200

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,4 +1,6 @@
-import os
-os.system("apt-get install -y unzip wget openssh-server && wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok-stable-linux-amd64.zip && mv ./ngrok /usr/bin/ngrok && service ssh start")
-os.system("ngrok authtoken 1pTJd3HRqdzzZPwHIWGSkrgEtLN_41A7CQFfeAKcnHa2ShxoX")
-os.system("ngrok tcp 22")
+!wget -nc https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
+!tar --skip-old-files -xvf tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
+!rm -f nohup.out; bash -ic 'nohup ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock new-session -d & disown -a' >/dev/null 2>&1
+!./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock wait tmate-ready
+!./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with SSH address: #{tmate_ssh}"
+!./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with web: #{tmate_web}"
